7aae314504e1093cea6acd59630ae5111d2d2c2b
yfksm <szy.happyisland.glay@gmail.com>
2021-08-31 23:41:42 +0900

    add login process

--- script/get_all_testcase.py
+++ script/get_all_testcase.py
@@ -2,11 +2,15 @@ import sys
 import os
 import requests
 from bs4 import BeautifulSoup
+import config
 
 CONTEST_NAME_CONF = os.path.dirname(__file__) + "/../conf/contest_name"
 PROBLEM_NAME_CONF = os.path.dirname(__file__) + "/../conf/problem"
 SAMPLE_INPUT_DIR  = os.path.dirname(__file__) + "/../sample_input"
 SAMPLE_OUTPUT_DIR = os.path.dirname(__file__) + "/../sample_output"
+
+LOGIN_URL = "https://atcoder.jp/login"
+CONTEST_URL = "https://atcoder.jp/contests/"
 
 with open(CONTEST_NAME_CONF) as f:
     contest_name = f.read().splitlines()[0]
@@ -16,7 +20,24 @@ with open(PROBLEM_NAME_CONF) as f:
     problem_name = f.read().splitlines()[0]
     print(problem_name)
 
-load_url = "https://atcoder.jp/contests/" + contest_name + "/tasks/" + contest_name + "_" + problem_name
+session = requests.session()
+
+csrf_soup = BeautifulSoup(session.get(LOGIN_URL).text, 'lxml')
+csrf_token = csrf_soup.find(attrs={'name': 'csrf_token'}).get('value')
+
+login_info = {
+    "csrf_token": csrf_token,
+    "username": config.USERNAME,
+    "password": config.PASSWORD
+}
+
+result = session.post(LOGIN_URL, data=login_info)
+result.raise_for_status()
+if result.status_code!=200:
+    print("Login Failed.")
+    exit(0)
+
+load_url = CONTEST_URL + contest_name + "/tasks/" + contest_name + "_" + problem_name
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
 
